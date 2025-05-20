2f2d8c853f92d73d0ab9668a0975c7c8bc352af4
DevuKumar27 <devukumarms.2325@gmail.com>
2025-05-20 20:22:33 +0530

    Add files via upload

---
+++ average_industry_compensation.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.average_industry_compensation ("Location","Role","Average Industry Compensation") VALUES
+	 ('Jaipur','Manager',2337356.0),
+	 ('Jaipur','Senior Associate',1558238.0),
+	 ('Jaipur','Associate',1038825.0),
+	 ('Jaipur','Senior Analyst',769500.0),
+	 ('Jaipur','Analyst',570000.0),
+	 ('Pune','Manager',2583394.0),
+	 ('Pune','Senior Associate',1722263.0),
+	 ('Pune','Associate',1148175.0),
+	 ('Pune','Senior Analyst',850500.0),
+	 ('Pune','Analyst',630000.0);
+INSERT INTO public.average_industry_compensation ("Location","Role","Average Industry Compensation") VALUES
+	 ('Banglore','Manager',2706413.0),
+	 ('Banglore','Senior Associate',1804275.0),
+	 ('Banglore','Associate',1202850.0),
+	 ('Banglore','Senior Analyst',891000.0),
+	 ('Banglore','Analyst',660000.0);
